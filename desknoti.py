babee1c4a92cc315ec15552ce5b4fec336564a47
tshivanshu9 <tshivanshu9@gmail.com>
2021-04-18 23:34:35 +0530

    third commit

--- desknoti.py
+++ desknoti.py
@@ -11,13 +11,13 @@ if covidData!=None:
     data=covidData.json()['Success']
     while True:
         notification.notify(
-            title="COVID19 Stats on {}".format(datetime.date.today()),
-            message="Total cases:{totalcases}\nToday cases:{todaycases}\nToday deaths:{todaydeaths}\nTotal active:{active}".format(
+            title="COVID 19 Stats on {}".format(datetime.date.today()),
+            message="Total cases : {totalcases}\nToday cases : {todaycases}\nToday deaths : {todaydeaths}\nTotal active : {active}".format(
                 totalcases=data['cases'],
                 todaycases=data['todayCases'],
                 todaydeaths=data['todayDeaths'],
                 active=data["active"]),
-            app_icon="Paomedia-Small-N-Flat-Bell.ico",
+            app_icon="covid.ico",
             timeout=50
         )
         time.sleep(60*60*4)
